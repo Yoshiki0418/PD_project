fb4ba5838d397d0e56609b86fff8fe71db3c0aa9
Yamamoto Yoshiki <yamamoto116@yamamotonoMacBook-Air.local>
2023-11-18 13:21:51 +0900

    レシピの詳細ページに遷移できるように準備

--- app.py
+++ app.py
@@ -200,6 +200,14 @@ def get_recipes():
     
     # JSONとしてレシピデータを返す
     return jsonify(recipes_data)
+
+@app.route('/recipe-details/<recipe_id>')
+def recipe_details(recipe_id):
+    recipe_id = request.args.get('id')  # URLパラメータからレシピIDを取得
+    print(recipe_id)
+    # レシピIDに基づいてデータを取得
+    recipe_data = ...  # レシピデータの取得ロジック
+    return render_template('recipe_details.html', recipe=recipe_data)
  
 
 
