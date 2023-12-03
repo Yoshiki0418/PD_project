5469715f7d379a3dee2c75d8f0ca604bc5b0fc79
Yoshiki0418 <c1206618@st.kanazawa-it.ac.jp>
2023-12-03 15:09:57 +0900

    スクレイピングした内容を返す

--- scraping.py
+++ scraping.py
@@ -1,76 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
 
-def scraping(input_ingredients , recipe_add):
-    # 入力された食材名を「、」で区切るようにする
+def scraping(input_ingredients, recipe_add):
     ingredients = input_ingredients.replace(",", "、")
-
-    # スクレイピングするURL
     url = f'https://cookpad.com/search/材料：{ingredients}'
-
-    # ページの内容を取得
     response = requests.get(url)
-
-    # BeautifulSoupオブジェクトを作成し、HTMLをパース
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    """
-    'class'が'recipe-image'のすべての'div'要素を検索し、それぞれの中の'a'タグから'href'を取得
-    これで、複数のページIDを取得することができる。
-    """
     recipe_image_divs = soup.find_all('div', class_='recipe-image')
-
-    # hrefのリストを取得
     hrefs = [div.find('a').get('href') for div in recipe_image_divs if div.find('a')]
 
-    cnt = 0
-    for href in hrefs:
-        cnt += 1
-        print(href)
+    recipes = []
+    for href in hrefs[:recipe_add]:
+        recipe_url = f"https://cookpad.com{href}"
+        response = requests.get(recipe_url)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-        if href:
-            # hrefがNoneでない場合のみ処理を実行
-            recipe_url = f"https://cookpad.com{href}"  # URLの形式に注意
-            response = requests.get(recipe_url)
-            soup = BeautifulSoup(response.text, 'html.parser')
+        recipe_info = {}
 
-            # タイトルの取得。タイトルが存在しない場合のためにNoneチェックを行う
-            title_tag = soup.find('h1', class_='recipe-title')
-            if title_tag:
-                title = title_tag.get_text(strip=True)
-                print(title)
+        title_tag = soup.find('h1', class_='recipe-title')
+        if title_tag:
+            recipe_info['title'] = title_tag.get_text(strip=True)
 
-            explanation = soup.find("div", class_="description_text")
-            if explanation:
-                explanation = explanation.get_text(strip=True)
-                print(explanation)
+        explanation = soup.find("div", class_="description_text")
+        if explanation:
+            recipe_info['explanation'] = explanation.get_text(strip=True)
 
-                # Find the image tag
-                img_tag = soup.find('img', class_="photo large_photo_clickable")
+        img_tag = soup.find('img', class_="photo large_photo_clickable")
+        if img_tag:
+            recipe_info['image_path'] = img_tag.get('src')
 
-                # Extract the image path from the src attribute of the img tag
-                image_path = img_tag.get('src')
-                print(image_path)
+        ingredient_names = soup.find_all('span', class_='name')
+        recipe_info['ingredients'] = [name.text for name in ingredient_names]
 
-            ingredient_names = soup.find_all('span', class_='name')
-            extracted_names = [name.text for name in ingredient_names]
-            count = 0
-            print("[材料]")
-            for name in extracted_names:
-                count += 1
-                print(f"{count}:{name}")
-            count = 0
-            
-            procedures = soup.find_all("p", class_="step_text")
-            procedures = [procedure.text for procedure in procedures]
-            print("[作り方]")
-            for procedure in procedures:
-                count += 1
-                print(f"{count}:{procedure}")
-            print("")
+        procedures = soup.find_all("p", class_="step_text")
+        recipe_info['procedures'] = [procedure.text for procedure in procedures]
 
-            if(recipe_add == cnt):
-                break
+        recipes.append(recipe_info)
+
+    return recipes
+
+# 使用例
+input_ingredients = "にんじん,大根"
+recipe_add = 2
+result = scraping(input_ingredients, recipe_add)
+for recipe in result:
+    print(recipe)
 
 
-scraping("にんじん,大根",4)
